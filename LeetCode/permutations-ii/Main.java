c821a5053804f986a26602607ff15901eb2d223e
SQC <1803510619@qq.com>
2017-12-06 11:50:10 +0800

    permutations-ii

---
+++ LeetCode/permutations-ii/Main.java
@@ -0,0 +1,48 @@
+题目描述
+
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+For example,
+[1,1,2]have the following unique permutations:
+[1,1,2],[1,2,1], and[2,1,1].
+import java.util.*;
+public class Solution {
+    ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+    HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>();
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        if (num == null || num.length == 0){
+            return ret;
+        }
+        Arrays.sort(num);
+        int n = num.length;
+        dfs(0,n,num);
+        Iterator<ArrayList<Integer>> it = set.iterator();
+        while(it.hasNext()){
+            ret.add(it.next());
+        }
+        return ret;
+    }
+    public void dfs(int t, int n, int[] num){
+        if (t >= n){
+            return;
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0;i<n;i++){
+            list.add(num[i]);
+        }
+        set.add(list);
+        for(int i = t+1;i<n;i++){
+            if (num[t] != num[i]){
+                swap(num,t,i);
+                dfs(t+1,n,num);
+                swap(num,t,i);
+            }
+        }
+        dfs(t+1,n,num);
+    }
+    
+    public void swap(int[] num,int i,int j){
+        int t = num[i];
+        num[i] = num[j];
+        num[j] = t;
+    }
+}
