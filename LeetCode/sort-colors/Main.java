4f05cda55bddd3cfe30d95206ed5a2d869c0b15d
SQC <1803510619@qq.com>
2018-06-01 10:11:20 +0800

    sort-colors

--- LeetCode/sort-colors/Main.java
+++ LeetCode/sort-colors/Main.java
@@ -1,6 +1,7 @@
 题目描述
 Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
 public class Solution {
     public void sortColors(int[] A) {
         if (A == null || A.length == 0){
@@ -24,7 +25,6 @@ public class Solution {
                 A[cur] = t;
                 r--;
             }
-        }
-        
+        } 
     }
 }
