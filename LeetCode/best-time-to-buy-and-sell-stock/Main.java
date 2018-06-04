7bdd99e62f3ea017efe3005efb37e9d98618460e
SQC <1803510619@qq.com>
2018-06-04 10:05:00 +0800

    best-time-to-buy-and-sell-stock

--- LeetCode/best-time-to-buy-and-sell-stock/Main.java
+++ LeetCode/best-time-to-buy-and-sell-stock/Main.java
@@ -2,6 +2,7 @@
 
 Say you have an array for which the i th element is the price of a given stock on day i.
 If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
 public class Solution {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length == 0){
