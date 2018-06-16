de6a89c72224112477cdddccef2260fe7baa4b76
SQC <1803510619@qq.com>
2018-06-16 15:36:23 +0800

    copy-list-with-random-pointer

--- LeetCode/copy-list-with-random-pointer/Main.java
+++ LeetCode/copy-list-with-random-pointer/Main.java
@@ -2,6 +2,14 @@
 
 A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 Return a deep copy of the list.
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
         if (head == null){
@@ -9,17 +17,21 @@ public class Solution {
         }
         RandomListNode p = new RandomListNode(-1);
         RandomListNode q = new RandomListNode(head.label);
+        RandomListNode s = p;
         p.next = q;
+        p = p.next;
         RandomListNode t = head;
-        while(head.next != null){
-            p.next = new RandomListNode(head.next.label);
+        while(head != null){
             if (head.random != null){
                 p.random = new RandomListNode(head.random.label);
+            }
+            if (head.next != null){
+                p.next = new RandomListNode(head.next.label);
             }
             p = p.next;
             head = head.next;
         }
         
-        return t;
+        return s.next;
     }
 }
