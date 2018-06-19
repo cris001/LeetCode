31ea9b9beb5982fce293e15b8bd202ed0f0c5845
SQC <1803510619@qq.com>
2018-06-19 17:26:29 +0800

    remove-duplicates-from-sorted-array-ii

--- LeetCode/remove-duplicates-from-sorted-array-ii/Main.java
+++ LeetCode/remove-duplicates-from-sorted-array-ii/Main.java
@@ -5,6 +5,7 @@ What if duplicates are allowed at most twice?
 For example,
 Given sorted array A =[1,1,1,2,2,3],
 Your function should return length =5, and A is now[1,1,2,2,3].
+
 public class Solution {
     public int removeDuplicates(int[] A) {
         if (A == null || A.length == 0){
