ba0e8be37542ef0ba85946bf388484461a31583b
SQC <1803510619@qq.com>
2018-07-16 23:16:56 +0800

    jump-game

--- LeetCode/jump-game/Main.java
+++ LeetCode/jump-game/Main.java
@@ -1,11 +1,11 @@
 题目描述
-
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
 Each element in the array represents your maximum jump length at that position.
 Determine if you are able to reach the last index.
 For example:
 A =[2,3,1,1,4], returntrue.
 A =[3,2,1,0,4], returnfalse.
+
 public class Solution {
     public boolean canJump(int[] A) {
         if (A == null || A.length == 0){
