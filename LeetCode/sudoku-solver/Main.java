59a41a275c2958cb8cd9c60fe3dd7c01eb6f2c4b
SQC <1803510619@qq.com>
2018-08-05 23:11:09 +0800

    sudoku-solver

--- LeetCode/sudoku-solver/Main.java
+++ LeetCode/sudoku-solver/Main.java
@@ -1,5 +1,4 @@
 题目描述
-
 Write a program to solve a Sudoku puzzle by filling the empty cells.
 Empty cells are indicated by the character'.'.
 You may assume that there will be only one unique solution.
